f7f99c25b93a6859f41ce1a46164c8e2055b75f2
zhoylman <36487713+zhoylman@users.noreply.github.com>
2019-02-25 16:53:26 -0700

    Working on best way to clip

--- functions/SPI_Map.R
+++ functions/SPI_Map.R
@@ -1,3 +1,5 @@
+rm(list = ls())
+
 ## LOAD THE REQUIRED LIBRARYS
 library(ncdf4) # Downlaoded from https://github.com/pmjherman/r-ncdf4-build-opendap-windows
 library(lubridate)
@@ -8,7 +10,10 @@ library(rowr)
 library(precintcon)
 library(gridExtra)
 library(raster)
+library(MASS)
+library(stars)
 
+tic()
 
 ## DEFINE OUR VARIABLE NAME 
 var="precipitation_amount"
@@ -16,6 +21,12 @@ var="precipitation_amount"
 raster_precip = brick("http://thredds.northwestknowledge.net:8080/thredds/dodsC/agg_met_pr_1979_CurrentYear_CONUS.nc", var= var)
 
 time_scale = 30
+
+montana = rgdal::readOGR("D:\\Git_Repo\\drought_indicators\\montana_outline.kml")
+
+# tic()
+# raster_precip_clipped_1 = crop(raster_precip, extent(montana))
+# toc()
 
 time = data.frame(datetime = as.POSIXct(as.Date(as.numeric(substring(names(raster_precip),2)), origin="1900-01-01")))
 time$day = yday(time$datetime)
@@ -48,37 +59,46 @@ for(j in 1:length(first_date_breaks)){
   }
 }
 
+
+#raster_precip_time_clip = raster_precip[[slice_vec]]
+
 library(doParallel)
 library(foreach)
-
-#summing first here
 
 cl = makeCluster(detectCores()-1)
 registerDoParallel(cl)
 
 tic()
-
-raster_precip_clipped = foreach(i=1:41) %dopar% {
+raster_precip_clipped = foreach(i=unique(group_by_vec)) %dopar% {
   library(raster)
   temp = sum(raster_precip[[slice_vec[group_by_vec == i]]])
-  crop(temp, extent(montana))
+  temp = crop(temp, extent(montana))
+  mask(temp, montana)
 }
-
 toc()
 
 stopCluster(cl)
 
 #243 on 7 cores seconds
 
-test = data.frame(matrix(nrow = length(values(raster_precip_clipped[[1]])), ncol = 41))
-for(i in 1:41){
+
+
+test = data.frame(matrix(nrow = length(values(raster_precip_clipped[[1]])), ncol = length(unique(group_by_vec))))
+for(i in 1:length(unique(group_by_vec))){
   test[,i] = values(raster_precip_clipped[[i]])
 }
 
 
+
+# x = as.numeric(test[1,])
+# 
+# test1 = fitdist(x, distr = "gamma", method = "mle")
+# test = MASS::fitdistr(x, "gamma")
+
+
 spi_fun <- function(x) { 
 
-  fit.gamma = tryCatch(fitdist(x, distr = "gamma", method = "mle"), error=function(e) NA)
+  fit.gamma = tryCatch(MASS::fitdistr(x, "gamma"), error=function(e) NA)
   if(is.na(fit.gamma)){
     return(NA)
   }
@@ -89,135 +109,15 @@ spi_fun <- function(x) {
   }
 }
 
+
 test_spi = apply(test, 1, spi_fun)
+
 
 test_spi_map = raster_precip_clipped[[1]]
 
 values(test_spi_map) = test_spi
 
-
-
-
-
-x = as.numeric(test[1,])
-
-fit.gamma = fitdist(x, distr = "gamma", method = "mle")
-fit.cdf = pgamma(x, fit.gamma$estimate[1], fit.gamma$estimate[2])
-standard_norm = qnorm(fit.cdf, mean = 0, sd = 1)
-
-
-
-
-
-
-
-
-x1 <- calc(s, fun)
-
-
-
-fit.gamma <- fitdist(data_time_filter$sum, distr = "gamma", method = "mle")
-
-#calculate modeled cdf
-fit.cdf = pgamma(data_time_filter$sum, fit.gamma$estimate[1], fit.gamma$estimate[2])
-
-par(mfrow = c(1,2))
-cdfcomp(fit.gamma)
-plot(data_time_filter$sum,fit.cdf)
-
-#CLOSE but do this for the gamma fit!!!
-#standard_norm raw data
-standard_norm = qnorm(ecdf(data_time_filter$sum)(data_time_filter$sum), mean = 0, sd = 1)
-
-#standard_norm modelled data
-#this is it!!!
-standard_norm2 = qnorm(fit.cdf, mean = 0, sd = 1)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# try cropping all maps first
-
-cl = makeCluster(detectCores())
-registerDoParallel(cl)
-
-tic()
-
-raster_precip_clipped = foreach(i=1:41) %dopar% {
-  library(raster)
-  temp = sum(raster_precip[[slice_vec[group_by_vec == i]]])
-  crop(temp, extent(montana))
-}
+plot(test_spi_map)
 
 toc()
 
-stopCluster(cl)
-
-
-
-
-
-
-#try cropping first here. 
-
-cl = makeCluster(detectCores())
-registerDoParallel(cl)
-
-tic()
-
-raster_precip_clipped = foreach(i=1:41) %dopar% {
-  library(raster)
-  temp = sum(raster_precip[[slice_vec[group_by_vec == i]]])
-  crop(temp, extent(montana))
-}
-
-toc()
-
-stopCluster(cl)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-test_clipped = crop(test, extent(montana))
-
-
-time_clipped_raster = raster[[slice_vec]]
-
-
-montana = rgdal::readOGR("D:\\Git_Repo\\drought_indicators\\montana_outline.kml")
-
-tic()
-clipped = crop(raster, extent(montana))
-toc()
-
-#example code of how I will agregate monthly data?
-# precip_test = time_clipped_raster %>% group_by(group_by_vec) %>%
-#   summarize(sum = sum(time_clipped_raster, na.rm = TRUE))
