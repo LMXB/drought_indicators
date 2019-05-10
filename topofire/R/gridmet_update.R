01d5f1df60645ca6da970d953553c6b61d261a19
Zach Hoylman <zhoylman@gmail.com>
2019-05-10 15:47:05 -0600

    updating topofire codebase

--- topofire/R/gridmet_update.R
+++ topofire/R/gridmet_update.R
@@ -1,35 +1,37 @@
-library(ncdf4)
-library(raster)
-library(doParallel)
-library(foreach)
-library(rgdal)
-library(parallel)
-library(XML)
-library(RCurl)
-
 #update gridmet data
-climatology.dir = "/mnt/ScratchDrive/data/Hoylman/gridMET_Climatology/gridMET_precip_raw"
+#this script runs 2 main operations
+# 1. fill in any missing data in the on disk climatology
+# 2. remove and recalulate the last 4 months of data (cant find information in meta data about provisional)
 
-files = list.files(climatology.dir, pattern = ".tif$", full.names = T)
+#set climatology dir and git dirs
+git.dir = "/home/zhoylman/drought_indicators/zoran/R/"
+climatology.dir = "/mnt/ScratchDrive/data/Hoylman/gridMET_Climatology/gridMET_precip_2.5km/"
 
-#compute time from files
-time_fdates = fdates(files)
-time = data.frame(datetime = as.Date(fdates(files), format = "%Y%m%d"))
-time$day = strftime(time$datetime,"%m-%d")
+#import master grid (needed for gridmet update function)
+master_grid = raster("/home/zhoylman/drought_indicators/zoran/master_warp_grid/master_grid_clipped_2.5km.tif")
 
-#bring in netcdf
-url = "http://thredds.northwestknowledge.net:8080/thredds/dodsC/agg_met_pr_1979_CurrentYear_CONUS.nc"
-netcdf = nc_open(url)
-test = ncvar_get(netcdf, "status")
+#import gridmet update function
+source(paste0(git.dir,"gridmet_update_fun.R"))
 
+#first fill in any missing data
+suppressWarnings(update_gridMET(climatology.dir, git.dir, master_grid))
 
+# now we will delete the last 4 months of data from the climatology.dir and re-fill
+# this is a nieve way of updating gridmet data once data is updated from provisional 
+# to perminant. From gridMET docs: 
 
-gridmet_precip = brick(url, var= "precipitation_amount")
-crs(gridmet_precip) <- "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0" 
+# "At first, assets are ingested with status='early'. After several days, they are 
+# replaced by assets with status='provisional'. After about 2 months, they are replaced 
+# by the final assets with status='permanent'."
 
-url2 = paste0("https://www.northwestknowledge.net/metdata/uncompressed/provisional/",2018,"/")
+#import file names
+files_updated = list.files(climatology.dir, pattern = ".tif$", full.names = T)
 
-result <- getURL(url2,verbose=F,ftp.use.epsv=T, dirlistonly = T)
-fnames <- getHTMLLinks(result,relative=F)
-fnames <- fnames[grep("gridmet_",fnames)]
-download_links <- paste0(url,fnames)
+#index files to delete (4 months = 120 days)
+files_to_remove = files_updated[(length(files_updated)-119):length(files_updated)]
+
+#remove files
+file.remove(files_to_remove)
+
+#fill in dataset with up to date data
+suppressWarnings(update_gridMET(climatology.dir, git.dir, master_grid))
