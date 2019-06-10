89684a0f4d735e3ad57c3b1d2ef74c969b6f5fca
Zach Hoylman <zhoylman@gmail.com>
2019-06-10 17:29:26 -0600

    good simple

--- shp_kml/R/UMRB_Clip.R
+++ shp_kml/R/UMRB_Clip.R
@@ -17,16 +17,15 @@ county_umrb = county %>%
   dplyr::filter(STATE_NAME == "Montana"|STATE_NAME == "Idaho"| STATE_NAME == "Wyoming"|STATE_NAME == "South Dakota"|STATE_NAME == "North Dakota")%>%
   sf::st_simplify()
 
-outline_umrb = st_union(states_umrb) %>%
+outline_umrb = st_combine(states_umrb) %>%
   sf::st_simplify()
+
+st_crs(outline_umrb) = 4326
 
 watersheds_umrb = watersheds %>%
-  st_intersection(county_umrb) %>%
-  sf::st_simplify()
+  st_intersection(outline_umrb) 
 
-watersheds_umrb = rmapshaper::ms_simplify(watersheds_umrb, keep = 0.05)
-
-watersheds_umrb = rmapshaper::ms_simplify(watersheds_umrb, keep = 0.01)
+watersheds_umrb_simple = rmapshaper::ms_simplify(watersheds_umrb, keep = 0.01)
 
 st_write(county_umrb, "/home/zhoylman/drought_indicators/shp_kml/larger_extent/county_umrb.shp", "county_umrb", driver = "ESRI Shapefile")
 st_write(outline_umrb, "/home/zhoylman/drought_indicators/shp_kml/larger_extent/outline_umrb.shp", "outline_umrb", driver = "ESRI Shapefile")
