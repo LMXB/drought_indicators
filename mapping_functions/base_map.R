51b65177ab92ddf527159f976848c9520e45b650
Zach Hoylman <zhoylman@gmail.com>
2019-08-14 11:28:20 -0600

    Validation Update

--- mapping_functions/base_map.R
+++ mapping_functions/base_map.R
@@ -26,29 +26,29 @@ pal_usdm_legend <- colorFactor(c("#ffff00", "#918151", "#ffa500", "#ff0000", "#8
                                                                                                     "D4 (Exceptional Drought)"))
 #define basemap function
 base_map = function(x){
-  leaflet(options = tileOptions(minZoom = 4, maxZoom = 10)) %>%
-    addMapPane("USDM", zIndex = 410) %>%
-    addTiles("https://maps.tilehosting.com/data/hillshades/{z}/{x}/{y}.png?key=KZO7rAv96Alr8UVUrd4a") %>%
+  leaflet::leaflet(options = leaflet::tileOptions(minZoom = 4, maxZoom = 10)) %>%
+    leaflet::addMapPane("USDM", zIndex = 410) %>%
+    leaflet::addTiles("https://maps.tilehosting.com/data/hillshades/{z}/{x}/{y}.png?key=KZO7rAv96Alr8UVUrd4a") %>%
     leaflet::addProviderTiles("Stamen.TonerLines") %>%
     leaflet::addProviderTiles("Stamen.TonerLabels") %>%
-    setMaxBounds( lng1 = -122
+    leaflet::setMaxBounds( lng1 = -122
                   , lat1 = 53
                   , lng2 = -91
                   , lat2 = 39)%>%
-    setView(lng = -108, lat = 45.5, zoom = 6) %>%
-    addPolygons(data = states, group = "States", fillColor = "transparent", weight = 2, color = "black", opacity = 1)%>%
-    addPolygons(data = current_usdm, group = "USDM", fillColor = ~pal_usdm(DM), weight = 2, opacity = 1, color = "black",
-                fillOpacity = 0.5, options = pathOptions(pane = "USDM"), highlight = 
-                  highlightOptions(weight = 5,color = "#666",fillOpacity = 0.7),label = labels_usdm[[1]], 
-                labelOptions = labelOptions(style = list("font-weight" = "normal", padding = "3px 8px"),textsize = "15px",direction = "auto"))%>%
-    addWMSTiles(
+    leaflet::setView(lng = -108, lat = 45.5, zoom = 6) %>%
+    leaflet::addPolygons(data = states, group = "States", fillColor = "transparent", weight = 2, color = "black", opacity = 1)%>%
+    leaflet::addPolygons(data = current_usdm, group = "USDM", fillColor = ~pal_usdm(DM), weight = 2, opacity = 1, color = "black",
+                fillOpacity = 0.5, options = leaflet::pathOptions(pane = "USDM"), highlight = 
+                  leaflet::highlightOptions(weight = 5,color = "#666",fillOpacity = 0.7),label = labels_usdm[[1]], 
+                labelOptions = leaflet::labelOptions(style = list("font-weight" = "normal", padding = "3px 8px"),textsize = "15px",direction = "auto"))%>%
+    leaflet::addWMSTiles(
       "http://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi", group = "Weather",
       layers = "nexrad-n0r-900913",
-      options = WMSTileOptions(format = "image/png", transparent = TRUE))%>%
-    addLayersControl(position = "topleft",
+      options = leaflet::WMSTileOptions(format = "image/png", transparent = TRUE))%>%
+    leaflet::addLayersControl(position = "topleft",
                      overlayGroups = c("USDM", "States", "Weather"),
-                     options = layersControlOptions(collapsed = FALSE)) %>%
-    addDrawToolbar(markerOptions = drawMarkerOptions(),
+                     options = leaflet::layersControlOptions(collapsed = FALSE)) %>%
+    leaflet::addDrawToolbar(markerOptions = drawMarkerOptions(),
                    polylineOptions = FALSE,
                    polygonOptions = FALSE,
                    circleOptions = FALSE,
@@ -57,7 +57,7 @@ base_map = function(x){
                    editOptions = FALSE,
                    singleFeature = TRUE,
                    targetGroup='draw')%>%
-    addLegend("bottomright", pal = pal_usdm_legend, values = c("D0 (Abnormally Dry)", "D1 (Moderate Drought)",
+    leaflet::addLegend("bottomright", pal = pal_usdm_legend, values = c("D0 (Abnormally Dry)", "D1 (Moderate Drought)",
                                                                "D2 (Severe Drought)", "D3 (Extreme Drought)",
                                                                "D4 (Exceptional Drought)"),title = "USDM")
 }
---
+++ validation/soil_moisture_old/R/soil_moisture_query.R
@@ -0,0 +1,57 @@
+library(tidyverse)
+library(DBI)
+library(magrittr)
+
+credentials = read.table("/home/zhoylman/env_var/mco_db.txt")
+
+mesonet_db <- DBI::dbConnect(odbc::odbc(),
+                             Driver = "ODBC Driver 17 for SQL Server",
+                             Server = "cfcsql17.gs.umt.edu",
+                             Database = "MCOMesonet",
+                             UID = (as.character(credentials$V1[1])),
+                             PWD = (as.character(credentials$V1[2])),
+                             Port = 1433)
+
+q <- "
+SET NOCOUNT ON
+DECLARE @raw observations_raw
+INSERT INTO @raw
+SELECT *
+FROM observations.raw
+WHERE (
+    DATEPART(hour, datetime AT TIME ZONE 'Mountain Standard Time') = 0 AND
+    DATEPART(minute, datetime AT TIME ZONE 'Mountain Standard Time') = 0 AND
+    -- logger_sn = '06-00186' AND
+    measurement = 'Water Content'
+)
+
+DECLARE @l0 observations_level_0
+INSERT INTO @l0
+SELECT *
+FROM api.get_L0(@raw)
+
+SELECT *
+FROM api.get_L1(@l0)
+"
+
+mesonet_soil_moisture <-
+  DBI::dbGetQuery(mesonet_db, q) %>%
+  tibble::as_tibble() %>%
+  dplyr::mutate(datetime =
+                  datetime %>%
+                  lubridate::as_datetime() %>%
+                  lubridate::with_tz("America/Denver") %>%
+                  lubridate::as_date()) %>%
+  dplyr::left_join(dplyr::tbl(mesonet_db,
+                              dbplyr::in_schema("meta","elements")) %>%
+                     dplyr::select(name,description_short) %>%
+                     dplyr::collect(),
+                   by = c("element" = "name")) %>%
+  dplyr::mutate_at(.vars = dplyr::vars(station_key,
+                                       element,
+                                       description_short,
+                                       units),
+                   .funs = ~factor(.))
+
+save(mesonet_soil_moisture, file = "/home/zhoylman/drought_indicators_data/mesonet_spei/mesonet_soil_moisture.RData")
+
