ad2934c088e49d99bed8b87f54708d1a5c9a5293
Zach Hoylman <zhoylman@gmail.com>
2019-10-16 13:01:30 -0600

    snotel app complete

--- snotel/precipitaiton_app/html_app.R
+++ snotel/precipitaiton_app/html_app.R
@@ -7,7 +7,7 @@ library(htmltools)
 library(lubridate)
 library(htmlwidgets)
 library(leaflet.esri)
-library(webshot)
+library(mapview)
 
 source("/home/zhoylman/drought_indicators/mapping_functions/base_map.R")
 
@@ -43,12 +43,14 @@ pal <- colorNumeric(c("red", "yellow", "green", "blue", "purple"), domain = c(mi
 #custom legend fix
 css_fix <- "div.info.legend.leaflet-control br {clear: both;}"
 
-plots = list.files("/home/zhoylman/drought_indicators/snotel/plots", full.names = T, pattern = "precip")
-plots_pngs = png::readPNG(plots[1])
-
-base_map() %>%
-addCircleMarkers(snotel$lon, snotel$lat, snotel$simple_id, popup = popupImage(plots), radius = 10, stroke = TRUE, fillOpacity = 0.9,
-               color = "black", fillColor = pal(daily_lookup$percent_crop)
+precip_map = base_map() %>%
+addCircleMarkers(snotel$lon, snotel$lat, snotel$simple_id, 
+                 #popup = popupImage(src = "remote",img = paste0("http://shiny.cfc.umt.edu:3838/drought_indicators/snotel/plots/precip_snotel_plot_",
+                #                                                snotel$simple_id,".png"), height = 300, width = 500),
+                 popup = paste0("<img src='http://shiny.cfc.umt.edu:3838/drought_indicators/snotel/plots/precip_snotel_plot_",
+                                snotel$simple_id,".png' height='400' width='600'/>"),
+                 radius = 10, stroke = TRUE, fillOpacity = 0.9,
+                 color = "black", fillColor = pal(daily_lookup$percent_crop)
 )%>%
 addLegend("bottomleft", pal = pal, values = daily_lookup$percent_crop,
         title = "% Average<br>Accumulated Precipitation<br> (Daily)",
@@ -56,6 +58,8 @@ addLegend("bottomleft", pal = pal, values = daily_lookup$percent_crop,
         na.label = "NA"
 )%>%
 setView(lng = -108, lat = 46.5, zoom = 6)
+
+htmlwidgets::saveWidget(precip_map, "/home/zhoylman/temp/test.html", selfcontained = T)
       
       
  
