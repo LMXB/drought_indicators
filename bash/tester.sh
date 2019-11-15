4cc321157fdae63cf0a30599a80b28eee0eace19
Zach Hoylman <zhoylman@gmail.com>
2019-11-15 12:06:26 -0700

    validation update

--- bash/tester.sh
+++ bash/tester.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-Rscript /home/zhoylman/drought_indicators/sedi_app/R/SEDI_Map.R
-Rscript /home/zhoylman/drought_indicators/sedi_app/R/build_html.R
+Rscript /home/zhoylman/drought_indicators/spei_app/R/SPEI_Map.R
+Rscript /home/zhoylman/drought_indicators/eddi_app/R/EDDI_Map.R
+
+Rscript /home/zhoylman/drought_indicators/spei_app/R/build_html.R
+Rscript /home/zhoylman/drought_indicators/eddi_app/R/build_html.R
