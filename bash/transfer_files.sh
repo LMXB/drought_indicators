86fa796cd5bba5b431cc6ad00bba3267ab8090f9
Zach Hoylman <zhoylman@gmail.com>
2020-04-14 09:44:24 -0600

    daily push

--- bash/transfer_files.sh
+++ bash/transfer_files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cp -r /home/zhoylman/drought_indicators/widgets /var/www/shiny.cfc.umt.edu/drought_indicators/ >/home/zhoylman/bash/log 2>&1
-cp -r /home/zhoylman/drought_indicators/snotel/plots /var/www/shiny.cfc.umt.edu/drought_indicators/ >/home/zhoylman/bash/log 2>&1
+cp -r /home/zhoylman/drought_indicators/widgets /var/www/shiny.cfc.umt.edu/drought_indicators/ 
+cp -r /home/zhoylman/drought_indicators/snotel/plots /var/www/shiny.cfc.umt.edu/drought_indicators/ 
 
-cp -r /home/zhoylman/drought_indicators/widgets /home/zhoylman/drought_indicators/docs/widgets
-cp -r /home/zhoylman/drought_indicators/snotel/plots /home/zhoylman/drought_indicators/docs/plots
+cp -r /home/zhoylman/drought_indicators/widgets /home/zhoylman/drought_indicators/docs/ >/home/zhoylman/bash/log 2>&1
+cp -r /home/zhoylman/drought_indicators/snotel/plots /home/zhoylman/drought_indicators/docs/ 
